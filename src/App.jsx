7f9be5cec214b4ff9e300fb0bf706d5b54f78f87
sibaonick <nikkisibawrai@example.com>
2025-03-15 13:44:36 +0545

    INITIAL COMMIT

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button!!!!!!
+    </button>
+  );
+}
+
+function App() {
+  return (
+    <>
+     
+    <MyButton />
+    </>
+  )
+}
+
+export default App
